5366ec9120b2fc668125d4701cd02e56cb8ee161
Dave Reed <dave256@mac.com>
2010-11-12 08:43:01 -0500

    use nil for string format parameter in tests

--- UnitTests/TestDASQLite.m
+++ UnitTests/TestDASQLite.m
@@ -49,17 +49,17 @@
     GHAssertEquals([db open], YES, @"failed to open database");
     
     [db beginTransaction];
-    GHAssertEquals([db executeUpdate:@"create table person (pkey integer primary key, lastName text, firstName text, position integer, aDate real)"], YES, @"create table failed");
-    GHAssertEquals([db executeUpdate:@"insert into person (lastName, firstName, position, aDate) values ('Reed', 'Dave', 2, 1289507894.9236939)"], YES, @"insert failed");
-    GHAssertEquals([db executeUpdate:@"insert into person (lastName, firstName, position, aDate) values ('Stroeh', 'John', 3, 1289517894.9236939)"], YES, @"insert failed");
-    GHAssertEquals([db executeUpdate:@"insert into person (lastName, firstName, position, aDate) values ('Anderson', 'Matt', 1, 1289515894.9236939)"], YES, @"insert failed");
+    GHAssertEquals([db executeUpdate:@"create table person (pkey integer primary key, lastName text, firstName text, position integer, aDate real)"], YES, nil);
+    GHAssertEquals([db executeUpdate:@"insert into person (lastName, firstName, position, aDate) values ('Reed', 'Dave', 2, 1289507894.9236939)"], YES, nil);
+    GHAssertEquals([db executeUpdate:@"insert into person (lastName, firstName, position, aDate) values ('Stroeh', 'John', 3, 1289517894.9236939)"], YES, nil);
+    GHAssertEquals([db executeUpdate:@"insert into person (lastName, firstName, position, aDate) values ('Anderson', 'Matt', 1, 1289515894.9236939)"], YES, nil);
     
-    GHAssertEquals([db executeUpdate:@"create table course (pkey integer primary key, name text, position integer)"], YES, @"create table failed");
-    GHAssertEquals([db executeUpdate:@"insert into course (name, position) values ('CS161', 2)"], YES, @"insert failed");
-    GHAssertEquals([db executeUpdate:@"insert into course (name, position) values ('CS160', 1)"], YES, @"insert failed");
+    GHAssertEquals([db executeUpdate:@"create table course (pkey integer primary key, name text, position integer)"], YES, nil);
+    GHAssertEquals([db executeUpdate:@"insert into course (name, position) values ('CS161', 2)"], YES, nil);
+    GHAssertEquals([db executeUpdate:@"insert into course (name, position) values ('CS160', 1)"], YES, nil);
 
     [db commit];
-    //GHAssertEquals([db commit], YES, @"failed to commit setUp");
+    //GHAssertEquals([db commit], YES, nil);
 }
 
 - (void)tearDown {
@@ -75,7 +75,7 @@
     for (Person *p in people) {
         //GHTestLog(@"%@", p);
     }
-    GHAssertEquals([people count], (NSUInteger)3, @"[people count] should be 3 for original file");
+    GHAssertEquals([people count], (NSUInteger)3, nil);
 }
 
 - (void)testSelectAllWhere {
@@ -87,13 +87,13 @@
         GHTestLog(@"%@", p);
     }
      */
-    GHAssertEquals([people count], (NSUInteger)2, @"[people count] should be 2 for original file with position <= 2");
+    GHAssertEquals([people count], (NSUInteger)2, nil);
 }
 
 - (void)testSelectOne {
     Person *p = [Person database:db selectOneWhere:@"where lastName='Reed'" orderBy:nil];
-    GHAssertEqualStrings(p.firstName, @"Dave", @"testSelectOne firstName");
-    GHAssertEqualStrings(p.lastName, @"Reed", @"testSelectOne lastName");
+    GHAssertEqualStrings(p.firstName, @"Dave", nil);
+    GHAssertEqualStrings(p.lastName, @"Reed", nil);
 }
 
 - (void)testInsert {
@@ -106,10 +106,10 @@
         GHAssertEquals(p.pkey, i, @"in theory this could be wrong if pkeys not in order");
     }
     NSArray *people = [Person database:db selectAllWhere:nil orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1003, @"[people count] should be 1003 after insert");
+    GHAssertEquals([people count], (NSUInteger)1003, nil);
 
     people = [Person database:db selectAllWhere:@"where firstName='John'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1001, @"[people count] should be 1001 for John");
+    GHAssertEquals([people count], (NSUInteger)1001, nil);
 }
 
 - (void)testInsertNoTransaction {
@@ -123,10 +123,10 @@
     }
     [db commit];
     NSArray *people = [Person database:db selectAllWhere:nil orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1003, @"[people count] should be 1003 after insert");
+    GHAssertEquals([people count], (NSUInteger)1003, nil);
     
     people = [Person database:db selectAllWhere:@"where firstName='John'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1001, @"[people count] should be 1001 for John");
+    GHAssertEquals([people count], (NSUInteger)1001, nil);
 }
 
 - (void)testUpdate {
@@ -136,11 +136,11 @@
     p1.position = 100;
     [p1 update:db];
     NSArray *people = [Person database:db selectAllWhere:@"where firstName='Matt'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1, @"[people count] should be 1 for Matt");
+    GHAssertEquals([people count], (NSUInteger)1, nil);
     Person *p2 = [Person database:db selectOneWhere:@"where firstName='Matt'" orderBy:nil];
-    GHAssertEqualStrings(p2.firstName, @"Matt", @"firstName");
-    GHAssertEqualStrings(p2.lastName, @"Lewis", @"lastName");
-    GHAssertEquals(p2.position, 100, @"position");
+    GHAssertEqualStrings(p2.firstName, @"Matt", nil);
+    GHAssertEqualStrings(p2.lastName, @"Lewis", nil);
+    GHAssertEquals(p2.position, 100, nil);
     
     p2.lastName = @"Anderson";
     p2.position = 1;
@@ -149,18 +149,18 @@
     [p2 insert:db];
     
     people = [Person database:db selectAllWhere:@"where firstName='Matt'" orderBy:@"order by lastName, firstName"];
-    GHAssertEquals([people count], (NSUInteger)2, @"[people count] should be 2 for Matt after insert");
+    GHAssertEquals([people count], (NSUInteger)2, nil);
     p1 = [people objectAtIndex:0];
     p2 = [people objectAtIndex:1];
-    GHAssertEqualStrings(p1.firstName, @"Matt", @"firstName");
-    GHAssertEqualStrings(p1.lastName, @"Anderson", @"lastName");
-    GHAssertEquals(p1.position, 1, @"position");
-    GHAssertEqualStrings([p1.aDate description], [now description], @"date should match after insert");
+    GHAssertEqualStrings(p1.firstName, @"Matt", nil);
+    GHAssertEqualStrings(p1.lastName, @"Anderson", nil);
+    GHAssertEquals(p1.position, 1, nil);
+    GHAssertEqualStrings([p1.aDate description], [now description], nil);
     
     
-    GHAssertEqualStrings(p2.firstName, @"Matt", @"firstName");
-    GHAssertEqualStrings(p2.lastName, @"Lewis", @"lastName");
-    GHAssertEquals(p2.position, 100, @"position");
+    GHAssertEqualStrings(p2.firstName, @"Matt", nil);
+    GHAssertEqualStrings(p2.lastName, @"Lewis", nil);
+    GHAssertEquals(p2.position, 100, nil);
 }
 
 - (void)testUpdateNoTransaction {
@@ -171,11 +171,11 @@
     p1.position = 100;
     [p1 updateNoTransaction:db];
     NSArray *people = [Person database:db selectAllWhere:@"where firstName='Matt'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1, @"[people count] should be 1 for Matt");
+    GHAssertEquals([people count], (NSUInteger)1, nil);
     Person *p2 = [Person database:db selectOneWhere:@"where firstName='Matt'" orderBy:nil];
-    GHAssertEqualStrings(p2.firstName, @"Matt", @"firstName");
-    GHAssertEqualStrings(p2.lastName, @"Lewis", @"lastName");
-    GHAssertEquals(p2.position, 100, @"position");
+    GHAssertEqualStrings(p2.firstName, @"Matt", nil);
+    GHAssertEqualStrings(p2.lastName, @"Lewis", nil);
+    GHAssertEquals(p2.position, 100, nil);
     
     p2.lastName = @"Anderson";
     p2.position = 1;
@@ -185,17 +185,17 @@
     [db commit];
     
     people = [Person database:db selectAllWhere:@"where firstName='Matt'" orderBy:@"order by lastName, firstName"];
-    GHAssertEquals([people count], (NSUInteger)2, @"[people count] should be 2 for Matt after insert");
+    GHAssertEquals([people count], (NSUInteger)2, nil);
     p1 = [people objectAtIndex:0];
     p2 = [people objectAtIndex:1];
-    GHAssertEqualStrings(p1.firstName, @"Matt", @"firstName");
-    GHAssertEqualStrings(p1.lastName, @"Anderson", @"lastName");
+    GHAssertEqualStrings(p1.firstName, @"Matt", nil);
+    GHAssertEqualStrings(p1.lastName, @"Anderson", nil);
     GHAssertEquals(p1.position, 1, @"position");
-    GHAssertEqualStrings([p1.aDate description], [now description], @"date should match after insert");
+    GHAssertEqualStrings([p1.aDate description], [now description], nil);
     
-    GHAssertEqualStrings(p2.firstName, @"Matt", @"firstName");
-    GHAssertEqualStrings(p2.lastName, @"Lewis", @"lastName");
-    GHAssertEquals(p2.position, 100, @"position");
+    GHAssertEqualStrings(p2.firstName, @"Matt", nil);
+    GHAssertEqualStrings(p2.lastName, @"Lewis", nil);
+    GHAssertEquals(p2.position, 100, nil);
 }
 
 - (void)testDelete {
@@ -209,18 +209,18 @@
     }
     [db commit];
     NSArray *people = [Person database:db selectAllWhere:nil orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1003, @"[people count] should be 1003 after insert");
+    GHAssertEquals([people count], (NSUInteger)1003, nil);
     
     people = [Person database:db selectAllWhere:@"where firstName='John'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1001, @"[people count] should be 1001 for John");
+    GHAssertEquals([people count], (NSUInteger)1001, nil);
     
     for (Person *p in people) {
         [p delete:db];
     }
     people = [Person database:db selectAllWhere:@"where firstName='John'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)0, @"[people count] should be 0 for John");
+    GHAssertEquals([people count], (NSUInteger)0, nil);
     people = [Person database:db selectAllWhere:nil orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)2, @"[people count] should be 2");
+    GHAssertEquals([people count], (NSUInteger)2, nil);
 }
 
 - (void)testDeleteNoTransaction {
@@ -234,10 +234,10 @@
     }
     [db commit];
     NSArray *people = [Person database:db selectAllWhere:nil orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1003, @"[people count] should be 1003 after insert");
+    GHAssertEquals([people count], (NSUInteger)1003, nil);
     
     people = [Person database:db selectAllWhere:@"where firstName='John'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)1001, @"[people count] should be 1001 for John");
+    GHAssertEquals([people count], (NSUInteger)1001, nil);
     
     [db beginTransaction];
     for (Person *p in people) {
@@ -245,9 +245,9 @@
     }
     [db commit];
     people = [Person database:db selectAllWhere:@"where firstName='John'" orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)0, @"[people count] should be 0 for John");
+    GHAssertEquals([people count], (NSUInteger)0, nil);
     people = [Person database:db selectAllWhere:nil orderBy:nil];
-    GHAssertEquals([people count], (NSUInteger)2, @"[people count] should be 2");
+    GHAssertEquals([people count], (NSUInteger)2, nil);
 }
 
 -(void)testSingleQuote {
@@ -277,11 +277,11 @@
 - (void)testSecondType {
     Course *c;
     c = [Course database:db selectOneWhere:nil orderBy:@"order by position"];
-    GHAssertEqualStrings(c.name, @"CS160", @"course should be CS160");
-    GHAssertEquals(c.position, 1, @"CS160 position should be 1");
+    GHAssertEqualStrings(c.name, @"CS160", nil);
+    GHAssertEquals(c.position, 1, nil);
     c = [Course database:db selectOneWhere:@"where position=2" orderBy:nil];
-    GHAssertEqualStrings(c.name, @"CS161", @"course should be CS161");
-    GHAssertEquals(c.position, 2, @"CS161 position should be 2");
+    GHAssertEqualStrings(c.name, @"CS161", nil);
+    GHAssertEquals(c.position, 2, nil);
 }
 
 /*
